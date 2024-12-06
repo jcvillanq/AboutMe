58282de7f8c75fa03d953a390699db70b8ee9401
jcvillanq <55392743+jcvillanq@users.noreply.github.com>
2024-12-06 17:52:10 +0100

    start

---
+++ src/com/company/VillanJuanCarlosMain.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class VillanJuanCarlosMain {
+
+    public static void main(String[] args) {
+	VillanJuanCarlosMain programa = new VillanJuanCarlosMain();
+	programa.inicio();
+    }
+
+    public void inicio(){
+
+        System.out.println("Bienvenido a nuestra beta ");
+        
+    }
+}
